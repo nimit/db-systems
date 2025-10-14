1b0355cca94b7cbb33fcf0c610a5393bfbb0c496
Nimit <nimit.shah622@gmail.com>
2025-10-14 19:18:54 +0000

    run.sh changes - added timeout in non-debug runs + check for test-suite pass

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 conda activate raft
 source .venv/bin/activate
 python3 waf configure build --enable-raft-test
-time build/deptran_server -f config/raft_lab_test.yml 2>&1 | tee output.txt
+timeout 10m build/deptran_server -f config/raft_lab_test.yml 2>&1 | tee output.txt
 # python3 waf configure build --enable-raft-test --debug
 # time gdb --args build/deptran_server -f config/raft_lab_test.yml 2>&1 | tee output-dbg.txt
+cat output.txt | grep PASSED
